392c631dd962ed9465c746f5419a5fb97c94cae0
srikanthpragada <srikanthpragada@gmail.com>
2025-03-13 20:53:16 +0530

    13-MAR

---
+++ commands.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+for n in range(1, 11):
+    print(n)
+print('Hello!')
+print('Hello!')
+print('Hello!')
+print('Hello!')
+print('Hello!')
+from datetime import datetime
+print(datetime.now())
+from datetime import datetime
+print(datetime.now())
+from datetime import datetime
+print(datetime.now())
+from datetime import datetime
+print(datetime.now())
+for n in range(1, 11):
+    print(n)
+datetime.today()
+get_ipython().run_line_magic('pinfo', 'datetime.strptime')
+get_ipython().run_line_magic('pinfo', 'datetime.strptime')
+get_ipython().system('dir')
+get_ipython().system('type names.txt')
+get_ipython().system('type names.txt')
+names = get_ipython().getoutput('type names.txt')
+names
+ext = "*.ipynb"
+get_ipython().system('dir {ext}')
+get_ipython().system('pip list')
+libs = get_ipython().getoutput('pip list')
+type(libs)
+libs
+len(libs)
+get_ipython().run_line_magic('pwd', '')
+get_ipython().run_line_magic('history', '')
+get_ipython().run_line_magic('save', "'commands.txt'")
+get_ipython().run_line_magic('save', 'commands.txt')
